04886fd8808e3bca186f6569021744f2c053f47d
Sergio de la Garza <sergio@delagarza.io>
2016-08-08 17:59:44 -0500

    Added scripty and testing environment

---
+++ tests/integration.js
@@ -0,0 +1,39 @@
+global.expect = require('chai').expect;
+global.sa = require('superagent');
+global.Promise = require('bluebird');
+
+var glob = require('glob');
+var Mocha = require('mocha');
+
+var mocha = new Mocha();
+mocha.reporter('spec');
+
+require(path.join(process.cwd(), '/bin/server.js'));
+
+Promise.resolve()
+  .then(function () {
+    glob.sync('test/integration/**/*.js')
+      .filter(function (filePath) {
+        var fileName = path.parse(filePath).base;
+
+        return (fileName.indexOf(process.argv[2]) !== -1)
+      })
+      .forEach(function (file) {
+        mocha.addFile(path.join(process.cwd(), file));
+      });
+
+    return Promise.resolve();
+  })
+  .then(function () {
+    // run Mocha
+    mocha.run(function (failures) {
+      process.on('exit', function () {
+        process.exit(failures);
+      });
+      process.exit();
+    });
+  })
+  .catch(function (err) {
+    logger.error(err, err.stack);
+    process.exit(1);
+  });
