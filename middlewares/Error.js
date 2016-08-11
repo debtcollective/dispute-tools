e19739445df97506322f54679d80e81485e8324c
Sergio de la Garza <sergio@delagarza.io>
2016-08-11 11:48:11 -0500

    lint Error middleware

--- middlewares/Error.js
+++ middlewares/Error.js
@@ -1,11 +1,8 @@
-module.exports = function(err, req, res, next) {
+/* globals logger */
+
+module.exports = (err, req, res, next) => {
   logger.error(err);
   logger.error(err.stack);
-
-  if (req.knex) {
-    req.knex.destroy(function () {});
-  }
-
 
   res.format({
     html() {
@@ -13,43 +10,57 @@ module.exports = function(err, req, res, next) {
         switch (err.name) {
           case 'NotFoundError':
 
-            return res.status(404).render('shared/404.pug', {
+            res.status(404).render('shared/404.pug', {
               message: err.message,
             });
+
             break;
           case 'ForbiddenError':
-            return res.status(403).render('shared/500.pug', {
-              error: err.stack
+            res.status(403).render('shared/500.pug', {
+              error: err.stack,
             });
+
             break;
           default:
-            return res.status(500).render('shared/500.pug', {
-              error: 'Error:\n\n' + JSON.stringify(err) + '\n\nStack:\n\n' + err.stack
+            res.status(500).render('shared/500.pug', {
+              error: `Error\n\n${JSON.stringify(err)} \n\nStack: ${err.stack}`,
             });
+
             break;
         }
       }
+
+      return res.status(500).render('shared/500.pug', {
+        error: `Error\n\n${JSON.stringify(err)} \n\nStack: ${err.stack}`,
+      });
     },
     json() {
       if (err.name) {
         switch (err.name) {
           case 'NotFoundError':
-            return res.status(404).json({
+            res.status(404).json({
               error: err.message,
             });
+
             break;
           case 'ForbiddenError':
-            return res.status(403).json({
-              error: err.message
+            res.status(403).json({
+              error: err.message,
             });
+
             break;
           default:
-            return res.status(500).json({
-              error: err.message
+            res.status(500).json({
+              error: err.message,
             });
+
             break;
         }
       }
-    }
+
+      return res.status(500).json({
+        error: err.message,
+      });
+    },
   });
 };
