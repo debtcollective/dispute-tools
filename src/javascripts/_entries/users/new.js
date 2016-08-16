94e027f30c43add0e5fcd385087853959c5ca106
Noel Delgado <auhcsei@gmail.com>
2016-08-16 17:04:35 -0500

    add / users new bundle

---
+++ src/javascripts/_entries/users/new.js
@@ -0,0 +1,17 @@
+import Header from '../../components/Header';
+import NewForm from '../../components/users/NewForm';
+
+class ViewUsersNew {
+  constructor(config) {
+    new Header({
+      currentUser: config.currentUser,
+      element: document.querySelector('[data-component-header]')
+    });
+
+    new NewForm({
+      element: document.querySelector('[data-component-usernewform]')
+    });
+  }
+};
+
+window.ViewUsersNew = ViewUsersNew;
