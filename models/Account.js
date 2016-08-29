9079b5dcc912b1ebb68831941430a6c97af330ff
Sergio de la Garza <sergio@delagarza.io>
2016-08-29 18:43:19 -0500

    Passing tests for Account Model

--- models/Account.js
+++ models/Account.js
@@ -1,24 +1,6 @@
 /* global Krypton, Class, CONFIG, AttachmentsProcessor, AWS, S3Uploader */
 
 const US_STATES = require('datasets-us-states-names');
-const path = require('path');
-require('s3-uploader');
-
-const imageProcessor = require(path.join(process.cwd(), 'lib', 'image-processor'));
-const LocalFS = require(path.join(process.cwd(), 'lib', 'node-attachment-processor-local-fs'));
-
-let uploader;
-
-// if (['production', 'staging'].indexOf(CONFIG.environment) !== -1) {
-  uploader = new S3Uploader(AWS, {
-    bucket: `thedebtcollective`,
-    pathPrefix: 'accounts',
-  });
-// } else {
-//   uploader = new LocalFS({
-//     pathPrefix: 'accounts',
-//   });
-// }
 
 const Account = Class('Account').inherits(Krypton.Model).includes(AttachmentsProcessor)({
   tableName: 'Accounts',
@@ -64,39 +46,6 @@ const Account = Class('Account').inherits(Krypton.Model).includes(AttachmentsPro
     'createdAt',
     'updatedAt',
   ],
-  PROCESSOR_VARIATIONS: [
-    {
-      matchMimeType: /image\/(jpe?g|png)/,
-      processor: imageProcessor.process,
-      name: 'tiny',
-      meta: {
-        maxWidth: 40,
-        maxHeight: 40,
-      },
-    },
-    {
-      matchMimeType: /image\/(jpe?g|png)/,
-      processor: imageProcessor.process,
-      name: 'small',
-      meta: {
-        maxWidth: 50,
-        maxHeight: 50,
-      },
-    },
-    {
-      matchMimeType: /image\/(jpe?g|png)/,
-      processor: imageProcessor.process,
-      name: 'medium',
-      meta: {
-        maxWidth: 170,
-        maxHeight: 170,
-      },
-    },
-  ],
-  uploaderInstance: uploader,
-  prototype: {
-
-  },
 });
 
 module.exports = Account;
