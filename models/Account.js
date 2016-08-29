10ddf3715c5c36ad8bf9b82574b97cfd6d66fc53
Sergio de la Garza <sergio@delagarza.io>
2016-08-29 12:56:15 -0500

    Add Account Model

---
+++ models/Account.js
@@ -0,0 +1,92 @@
+/* global Krypton, Class, CONFIG, AttachmentsProcessor, AWS, S3Uploader */
+
+const US_STATES = require('datasets-us-states-names');
+const path = require('path');
+require('s3-uploader');
+
+const imageProcessor = require(path.join(process.cwd(), 'lib', 'image-processor'));
+
+const uploader = new S3Uploader(AWS, {
+  bucket: `thedebtcollective-${CONFIG.environment}`,
+  pathPrefix: 'accounts',
+});
+
+const Account = Class('Account').inherits(Krypton.Model).includes(AttachmentsProcessor)({
+  tableName: 'Accounts',
+  states: US_STATES,
+  validations: {
+    userId: ['required'],
+    fullname: ['required'],
+    state: [
+      'required',
+      {
+        rule(val) {
+          if (this.states.indexOf(val) === -1) {
+            throw new Error('The Account\'s state is invalid.');
+          }
+        },
+        message: 'The Account\'s state is invalid.',
+      },
+    ],
+    zip: [
+      'required',
+      {
+        rule(val) {
+          if (/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(val) === false) {
+            throw new Error('The Account\'s zip code is invalid.');
+          }
+        },
+        message: 'The Account\'s zip code is invalid.',
+      },
+    ],
+  },
+  attributes: [
+    'id',
+    'userId',
+    'collectiveId',
+    'fullname',
+    'bio',
+    'state',
+    'zip',
+    'socialLinks',
+    'imagePath',
+    'imageMeta',
+    'createdAt',
+    'updatedAt',
+  ],
+  PROCESSOR_VARIATIONS: [
+    {
+      matchMimeType: /image\/(jpe?g|png)/,
+      processor: imageProcessor.process,
+      name: 'tiny',
+      meta: {
+        maxWidth: 40,
+        maxHeight: 40,
+      },
+    },
+    {
+      matchMimeType: /image\/(jpe?g|png)/,
+      processor: imageProcessor.process,
+      name: 'small',
+      meta: {
+        maxWidth: 50,
+        maxHeight: 50,
+      },
+    },
+    {
+      matchMimeType: /image\/(jpe?g|png)/,
+      processor: imageProcessor.process,
+      name: 'medium',
+      meta: {
+        maxWidth: 170,
+        maxHeight: 170,
+      },
+    },
+  ],
+  uploaderInstance: uploader,
+  prototype: {
+
+  },
+});
+
+module.exports = Account;
