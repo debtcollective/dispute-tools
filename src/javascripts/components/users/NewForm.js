fe4b8f4b79e3688f76fa1f697b010f8327216902
Noel Delgado <auhcsei@gmail.com>
2016-09-01 15:17:05 -0500

    add / join us form, validate zip code

--- src/javascripts/components/users/NewForm.js
+++ src/javascripts/components/users/NewForm.js
@@ -8,7 +8,13 @@ export default class UsersNewForm extends Widget {
       fullname: ['required'],
       collectiveId: ['required'],
       state: ['required'],
-      zip: ['required'],
+      zip: ['required', {
+        rule(val) {
+          if (/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(val) === false) {
+            throw new Error('The Account\'s zip code is invalid.');
+          }
+        },
+      }],
       email: ['required', 'email'],
       password: ['required', 'minLength:8'],
     };
