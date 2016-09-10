748de5b20df17a1601408e2ad4197d5a19313e87
Noel Delgado <auhcsei@gmail.com>
2016-09-09 19:00:20 -0500

    add / tools, about markdown and seeds

--- seeds/dispute_tools.js
+++ seeds/dispute_tools.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 exports.seed = (knex) => {
   const uuid = require('uuid');
 
@@ -6,7 +7,16 @@ exports.seed = (knex) => {
       const tools = [
         {
           name: 'Wage Garnishment Dispute',
-          about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          about: `### Wage Garnishment for Federal Student Loans Dispute
+
+If your wages are being garnished or if you received a letter threatening wage garnishment, you have a lot in common with thousands of other people whose wages are seized every year. This is wrong, since no one should have to go into debt for education!
+
+You can use this form to submit a dispute to the Department of Education (for Direct Loans) and to the guaranty agency (for FFEL loans). We streamlined this form for our members because the Department's form is unnecessarily complicated.
+
+PLEASE NOTE: **Before you begin the wage garnishment dispute process, you should find out who owns your student loans.**
+
+If you have Direct loans, we will send your dispute to the Department of Education. If you have FFEL loans, you will need to provide the name and address of the originator of your loan. If you don't know the name of the guarantor, you can call the Department of the Treasury and ask them for the name and address of your guarantor.<br/>
+The number is: 1-800-304-3107.`,
           completed: 0,
           data: {
             options: {
@@ -119,7 +129,7 @@ exports.seed = (knex) => {
                               ],
                             },
                           ],
-                        ]
+                        ],
                       },
                       {
                         title: 'Employment',
@@ -220,10 +230,18 @@ exports.seed = (knex) => {
         },
         {
           name: 'Tax Offset Dispute',
-          about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          about: `### Tax Offset for Federal Student Loans
+
+If your taxes are being seized ("offset") or if you have been threatened with offset, you have a lot in common with thousands of other people whose taxes are offset each year. This is wrong, since no one should have to go into debt for education!
+
+You can use this form to submit a dispute to the Department of Education. The Department's form is unnecessarily complicated. We have streamlined the process for our members. You can make sure your offset is placed on hold if you file this dispute by 65 days after the date you received the offset notice. You can also stop an offset that has already begun if you receive a favorable decision from the Department.
+
+**What if I suspect that my taxes will be offset but I haven't received a notice?**
+
+The Department of Education usually sends offset notices once per year in the summer. If you want to know whether your taxes will be offset as a result of a federal student loan, you can call the Bureau of the Fiscal Service at 1-800-304-3107. Making this call does not make it more likely that your taxes will be offset.`,
           completed: 0,
           data: {
-            options : {
+            options: {
               A: {
                 title: 'My school closed before I could complete my degree.',
                 description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
@@ -235,18 +253,24 @@ exports.seed = (knex) => {
                 description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
                 steps: [],
               },
-            }
+            },
           },
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
           name: 'General Debt Dispute Letter',
-          about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          about: `### General Debt Dispute Letter
+
+#### for any defaulted personal debt
+
+This tool is for anyone who is in default on a debt. If you have been contacted by a collector who bought your loan from the original lender, you can fill out this form. Most of the time, collectors can't prove they own your debt, so by disputing the loan you may be able to stop collections. Before you begin to use this tool, you should make sure you have the name and address of the collections agency that is attempting to collect from you.
+
+**If you don't have the name and address of the collection agency, you will not be able to file this dispute.**`,
           completed: 0,
           data: {
             options: {
-              'none': {
+              none: {
 
               },
             },
@@ -260,9 +284,11 @@ exports.seed = (knex) => {
         return {
           id: uuid.v4(),
           name: tool.name,
+          about: tool.about,
+          completed: tool.completed,
           data: tool.data,
-          created_at: new Date(),
-          updated_at: new Date(),
+          created_at: tool.created_at,
+          updated_at: tool.updated_at,
         };
       }));
     });
