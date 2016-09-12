f65700375e054748bff59bedcab957f4e423bc3d
Sergio de la Garza <sergio@delagarza.io>
2016-09-12 16:58:23 -0500

    4. Credit Report Dispute Letter - Added all options

--- seeds/dispute_tools.js
+++ seeds/dispute_tools.js
@@ -451,6 +451,181 @@ This tool is for anyone who is in default on a debt. If you have been contacted 
                       },
                     ],
                   },
+                  {
+                    type: 'information',
+                    name: 'info-dispute-letter',
+                    title: 'General Debt Dispute Letter',
+                    description: 'With your previous information we’ll fill this letter for you, you will receive copies at the end.',
+                    footer: 'This is an auto-generated form, you need to fill the {link to open the previous form} first.',
+                  },
+                  {
+                    type: 'upload',
+                    name: 'collection-notice-uploader',
+                    multiple: false,
+                    optional: false,
+                    mimeTypes: [/image/, 'application/pdf'],
+                    maxFileSize: 5242880,
+                    title: 'Collection notice',
+                    description: 'Attach a digital copy of the collection notice you received in the mail. You can take a photo of the notice with your phone or scan it into your computer.',
+                    uploadButtonText: 'Upload files',
+                  }
+                ],
+              },
+            },
+          },
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+
+        {
+          name: 'Credit Report Dispute Letter',
+          about: `### Credit Report Dispute Letter
+
+Before using this tool, you should get a copy of your credit report and review it for errors. Millions of us have errors on our credit reports, which make it harder to do basic things like get a job or rent an apartment. This is totally unfair and unacceptable!
+Fight back! You can get a free copy of your credit report once per year at [annualcreditreport.com](http://annualcreditreport.com) or call 1-877-322-8228. For a list of common errors to look for, go [here](http://google.com).
+
+Once you have determined that there are errors on your report, you can use this tool to ask for a correction. This tool will help you write a dispute letter to the three main credit reporting agencies. The Debt Collective will submit the letters your behalf. Consumer reporting agencies have 5 business days after completing an investigation to notify you of the results. Generally, they must investigate the dispute within 30 days of receiving it. For more information about the process, go [here](http://google.com).
+`,
+          completed: 0,
+          data: {
+            disputeProcess: 1,
+            options: {
+              none: {
+                title: 'Gather Materials',
+                description: 'A little known fact of the debt collections industry is that the vast majority of collectors can\'t prove they own our debts. If they can\'t prove it, then why should we pay? Demanding proof of ownership is the first step to getting debt collectors off our backs.',
+                steps: [
+                  {
+                    type: 'form',
+                    name: 'personal-information-form',
+                    title: 'Personal Information',
+                    description: 'Here we need some personal, school and employment information.',
+                    fieldSets: [
+                      {
+                        title: 'About You',
+                        fields: [
+                          {
+                            name: 'name',
+                            label: 'Your Full Name',
+                            validations: [
+                              'required',
+                              'alpha',
+                              'maxLength:128',
+                            ],
+                          },
+                          {
+                            name: 'address1',
+                            label: 'Your Address',
+                            validations: [
+                              'required',
+                              'maxLength:128',
+                            ],
+                          },
+                          [
+                            {
+                              name: 'address2',
+                              label: 'Your Address 2',
+                              validations: [
+                                'maxLength:128',
+                              ],
+                            },
+                            {
+                              name: 'state',
+                              label: 'Your State',
+                              type: 'dropdown',
+                              options: US_STATES,
+                              validations: [
+                                'required',
+                              ],
+                            },
+                          ],
+                          [
+                            {
+                              name: 'email',
+                              label: 'Your email',
+                              placeholder: 'you@example.com',
+                              columnClassName: '.one-half',
+                              validations: [
+                                'required',
+                                'email',
+                                'maxLength:128',
+                              ],
+                            },
+                            {
+                              name: 'phone',
+                              label: 'Your telephone',
+                              placeholder: '(55) 555-5555-5555',
+                              columnClassName: '.one-half',
+                              validations: [
+                                'required',
+                                'alphaNumeric',
+                                'maxLength:16',
+                              ],
+                            },
+                          ],
+                          {
+                            name: 'dob',
+                            type: 'date',
+                            label: 'Date of Birth',
+                            validations: [
+                              'date',
+                            ],
+                          },
+                          {
+                            name: 'ssn',
+                            label: 'Social Security #',
+                            validations: [
+                              'required',
+                            ],
+                          },
+                          {
+                            name: 'agencies',
+                            label: 'Please select which credit reporting agency or agencies you would like your dispute to go to',
+                            type: 'dropdown',
+                            multiple: true,
+                            options: [
+                              {
+                                Experian: 'Experian',
+                                Equifax: 'Equifax',
+                                TransUnion: 'TransUnion'
+                              },
+                            ],
+                            validations: [
+                              'required',
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                  {
+                    type: 'information',
+                    name: 'credit-dispute-letter',
+                    title: 'Credit Report Dispute Letter',
+                    description: 'With your previous information we’ll fill this letter for you, you will receive copies at the end.',
+                    footer: 'This is an auto-generated form, you need to fill the {link to open the previous form} first.',
+                  },
+                  {
+                    type: 'upload',
+                    name: 'picture-id-uploader',
+                    multiple: false,
+                    optional: false,
+                    mimeTypes: [/image/],
+                    maxFileSize: 5242880,
+                    title: 'Picture ID',
+                    description: 'Please attach a photo of your picture ID. ',
+                    uploadButtonText: 'Upload files',
+                  },
+                  {
+                    type: 'upload',
+                    name: 'credit-errors-uploader',
+                    multiple: true,
+                    optional: true,
+                    mimeTypes: [/image/, 'application/pdf'],
+                    maxFileSize: 5242880,
+                    title: 'Credit report errors (optional)',
+                    description: 'lease attach a document highlighting credit report errors.',
+                    uploadButtonText: 'Upload files',
+                  },
                 ],
               },
             },
