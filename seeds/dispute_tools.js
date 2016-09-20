03d1525e18c30fed6225c7fbdac5eaced5056157
Noel Delgado <auhcsei@gmail.com>
2016-09-19 20:32:39 -0500

    update / PRIVATE STUDENT LOAN DISPUTE text
    - We need to explain more about what a private student loan is (many
      people don’t know). Below is the text I included in the tool doc that
      I sent to you. This should appear somewhere, maybe in the “why is this
      important” section?

--- seeds/dispute_tools.js
+++ seeds/dispute_tools.js
@@ -1356,6 +1356,12 @@ The Department of Education usually sends offset notices once per year in the su
 
 #### For any defaulted private student loan debt that has been sent to collections.
 
+Private student loans are those issued by a company not working on behalf of the government. These companies often contract with a servicer to collect the debt. Some of them break the law when trying to collect. What is more, because of sloppy paperwork, the new owners of the debt often can’t prove they own it. Demanding proof of ownership, or “chain of title,” is the first step to getting debt collectors off our backs.
+
+If you are in default on a student loan, this tool will assist you to generate a letter that will be automatically sent to the debt collector asking them to prove that they have the right to collect on the debt they have been bugging you about. You have a right to this evidence. If the debt collector cannot prove that you owe them money, they will be less likely to sue you or continue to collect).
+
+This tool should **not** be used in response to a Complaint/Summons letter letting you know that the collector is actively seeking a judgment against you in court. If you are being sued, you should seek legal advice.
+
 This tool is for anyone who is in default on a debt. If you have been contacted by a collector who bought your loan from the original lender, you can fill out this form. Most of the time, collectors can't prove they own your debt, so by disputing the loan you may be able to stop collections. Before you begin to use this tool, you should make sure you have the name and address of the collections agency that is attempting to collect from you.
 
 If you don't have the name and address of the collection agency, you will not be able to file this dispute.
