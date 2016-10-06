cc8b26b9dc044ab0a188d2a65ff52bf2644e1520
Noel Delgado <auhcsei@gmail.com>
2016-10-06 16:51:08 -0500

    update / join us form, two steps

--- src/javascripts/components/users/NewForm.js
+++ src/javascripts/components/users/NewForm.js
@@ -6,7 +6,7 @@ export default class UsersNewForm extends Widget {
   static get constraints() {
     return {
       fullname: ['required'],
-      collectiveId: ['required'],
+      collectiveIds: ['required', 'array', 'minLength:1'],
       state: ['required'],
       zip: ['required', {
         rule(val) {
@@ -24,46 +24,98 @@ export default class UsersNewForm extends Widget {
     super(config);
 
     this.ui = {};
+    const _pluralRe = new RegExp(/s$/);
+    let query = '';
     Object.keys(UsersNewForm.constraints).forEach(key => {
-      const query = `[name="${key}"]`;
-      this.ui[key] = this.element.querySelector(query);
+      query = `[name="${key}"]`;
+      if (_pluralRe.test(key)) {
+        this.ui[key] = [].slice.call(this.element.querySelectorAll(query));
+      } else {
+        this.ui[key] = this.element.querySelector(query);
+      }
     });
     this._checkit = new Checkit(UsersNewForm.constraints);
 
     this.appendChild(new Button({
-      name: 'Button',
+      name: 'ButtonSubmit',
       element: this.element.querySelector('button'),
     }));
 
-    this._bindEvents();
+    this._setupStepOne()._bindEvents();
+  }
+
+  _setupStepOne() {
+    this.collectivesOptions = [].slice.call(
+      this.element.querySelectorAll('[name="collectiveIds"]')
+    );
+    this.step1Layer = this.element.querySelector('.js-step-1');
+    this.step2Layer = this.element.querySelector('.js-step-2');
+    this.nextBtn = this.element.querySelector('#sign-up-btn-next');
+    this.prevBtn = this.element.querySelector('#sign-up-btn-back');
+
+    this.appendChild(new Button({
+      name: 'ButtonContinue',
+      element: this.nextBtn,
+    }));
+
+    return this;
   }
 
   _bindEvents() {
+    this._handleCollectiveOptionsChangeRef = this._handleCollectiveOptionsChange.bind(this);
+    for (let i = 0, len = this.collectivesOptions.length; i < len; i++) {
+      this.collectivesOptions[i].addEventListener('change', this._handleCollectiveOptionsChangeRef);
+    }
+
+    this.nextBtn.addEventListener('click', this.next.bind(this));
+    this.prevBtn.addEventListener('click', this.prev.bind(this));
+
     this._handleFormSubmit = this._handleFormSubmit.bind(this);
     this.element.querySelector('form').addEventListener('submit', this._handleFormSubmit);
   }
 
+  _handleCollectiveOptionsChange() {
+    const selected = this.collectivesOptions
+      .filter(c => { return c.checked; })
+      .map(v => { return v.value; });
+
+    this.ButtonContinue[selected.length ? 'enable' : 'disable']();
+  }
+
+  next() {
+    this.step1Layer.setAttribute('aria-hidden', true);
+    this.step2Layer.setAttribute('aria-hidden', false);
+  }
+
+  prev() {
+    this.step1Layer.setAttribute('aria-hidden', false);
+    this.step2Layer.setAttribute('aria-hidden', true);
+  }
+
   _handleFormSubmit(ev) {
-    this.Button.disable();
+    this.ButtonSubmit.disable();
     this._clearFieldErrors();
 
     const [err] = this._checkit.validateSync(this._getFieldsData());
 
     if (err) {
       ev.preventDefault();
-      this.Button.enable();
+      this.ButtonSubmit.enable();
       return this._displayFieldErrors(err.errors);
     }
 
-    this.Button.updateText();
+    this.ButtonSubmit.updateText();
 
     return undefined;
   }
 
   _displayFieldErrors(errors) {
+    let parent;
+    let errorLabel;
+
     Object.keys(errors).forEach(key => {
-      const parent = this.ui[key].parentNode;
-      let errorLabel = parent.querySelector('.-on-error');
+      parent = this.ui[key].parentNode || this.ui[key][0].parentNode;
+      errorLabel = parent.querySelector('.-on-error');
 
       parent.classList.add('error');
 
@@ -80,15 +132,23 @@ export default class UsersNewForm extends Widget {
   }
 
   _clearFieldErrors() {
+    let parent;
     Object.keys(UsersNewForm.constraints).forEach(key => {
-      this.ui[key].parentNode.classList.remove('error');
+      parent = this.ui[key].parentNode || this.ui[key][0].parentNode;
+      parent.classList.remove('error');
     });
   }
 
   _getFieldsData() {
     const data = {};
     Object.keys(UsersNewForm.constraints).forEach(key => {
-      data[key] = this.ui[key].value;
+      if (this.ui[key] instanceof Array) {
+        data[key] = this.ui[key]
+          .filter(c => { return c.checked; })
+          .map(v => { return v.value; });
+      } else {
+        data[key] = this.ui[key].value;
+      }
     });
     return data;
   }
