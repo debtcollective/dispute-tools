7352fda15c47aafd2f96cedbf84bab7b4d62de09
Noel Delgado <auhcsei@gmail.com>
2016-10-14 18:34:21 -0500

    update / admin/disputes, pagination functionality

--- src/javascripts/components/admin/disputes/AdminDisputesIndexTableControls.js
+++ src/javascripts/components/admin/disputes/AdminDisputesIndexTableControls.js
@@ -15,6 +15,7 @@ export default class AdminDisputesIndexTableControls extends Widget {
     this.statusSelect = this.statusSelectParent.querySelector('[name="disputesListValue[status]"]');
     this.statusSelectOptions = [].slice.call(this.statusSelect.options);
 
+    this.applyFiltersBtn = this.element.querySelector('[name="disputesListValue[applyFilters]"]');
     this.resetFiltersBtn = this.element.querySelector('[name="disputesListValue[resetFilters]"]');
 
     this._bindEvents();
@@ -22,67 +23,37 @@ export default class AdminDisputesIndexTableControls extends Widget {
 
   _bindEvents() {
     this.searchInput.addEventListener('input', ev => {
-      const v = ev.target.value.toLowerCase();
-      this.searchInputParent.classList[v ? 'add' : 'remove']('-bg-accent');
-      this.dispatch('searchInput', { value: v });
+      this.dispatch('searchInput', {
+        value: ev.target.value.toLowerCase(),
+      });
     });
 
     this.toolsSelect.addEventListener('change', ev => {
-      const v = ev.target.value;
-      this.toolsSelectParent.classList[v ? 'add' : 'remove']('-bg-accent');
-      this.dispatch('toolsChange', { value: v });
+      this.dispatch('toolsChange', {
+        value: ev.target.value,
+      });
     });
 
     this.statusSelect.addEventListener('change', ev => {
-      const v = ev.target.value;
-      this.statusSelectParent.classList[v ? 'add' : 'remove']('-bg-accent');
-      this.dispatch('statusChange', { value: v });
+      this.dispatch('statusChange', {
+        value: ev.target.value,
+      });
+    });
+
+    this.applyFiltersBtn.addEventListener('click', () => {
+      this.dispatch('applyFilters');
     });
 
     this.resetFiltersBtn.addEventListener('click', () => {
-      this.searchInput.value = '';
-      this.toolsSelectOptions[0].selected = true;
-      this.statusSelectOptions[0].selected = true;
-
-      this.searchInputParent.classList.remove('-bg-accent');
-      this.toolsSelectParent.classList.remove('-bg-accent');
-      this.statusSelectParent.classList.remove('-bg-accent');
-
       this.dispatch('resetFilters');
     });
   }
 
-  updateToolsDropdownOptionsState(ids) {
-    for (let i = 1, len = this.toolsSelectOptions.length; i < len; i++) {
-      if (ids.indexOf(this.toolsSelectOptions[i].value) > -1) {
-        this.toolsSelectOptions[i].disabled = false;
-      } else {
-        this.toolsSelectOptions[i].disabled = true;
-      }
-    }
-
-    return this;
+  enableApplyButton() {
+    this.applyFiltersBtn.removeAttribute('disabled');
   }
 
-  updateStatusDropdownOptionsState(ids) {
-    for (let i = 1, len = this.statusSelectOptions.length; i < len; i++) {
-      if (ids.indexOf(this.statusSelectOptions[i].value) > -1) {
-        this.statusSelectOptions[i].disabled = false;
-      } else {
-        this.statusSelectOptions[i].disabled = true;
-      }
-    }
-
-    return this;
-  }
-
-  showResetButton() {
-    this.resetFiltersBtn.setAttribute('aria-hidden', false);
-    return this;
-  }
-
-  hideResetButton() {
-    this.resetFiltersBtn.setAttribute('aria-hidden', true);
-    return this;
+  disableApplyButton() {
+    this.applyFiltersBtn.setAttribute('disabled', true);
   }
 }
