9ccac2ebba955464161da14bc2bdce6bcb6de656
Sergio de la Garza <sergio@delagarza.io>
2016-10-26 12:29:29 -0500

    - Modify collective seed file to have static ids - Add CollectvesController

---
+++ controllers/CollectivesController.js
@@ -0,0 +1,63 @@
+/* globals CONFIG, Class, RestfulController, Collective, DisputeTool, */
+
+const CollectivesController = Class('CollectivesController').inherits(RestfulController)({
+  beforeActions: [
+    {
+      before: '_loadCollective',
+      actions: [
+        'show',
+      ],
+    },
+    {
+      before(req, res, next) {
+        Collective.query()
+          .include('tools')
+          .orderBy('created_at', 'DESC')
+          .then((collectives) => {
+            req.collectives = collectives;
+            res.locals.collectives = collectives;
+            next();
+          })
+          .catch(next);
+      },
+      actions: ['index'],
+    },
+    {
+      before(req, res, next) {
+        DisputeTool.query()
+          .orderBy('created_at', 'ASC')
+          .then((tools) => {
+            req.disputeTools = tools;
+            res.locals.disputeTools = tools;
+            next();
+          })
+          .catch(next);
+      },
+      actions: ['index', 'show'],
+    },
+  ],
+  prototype: {
+    _loadCollective(req, res, next) {
+      Collective.query()
+        .where({ id: req.params.id })
+        .include('tools')
+        .then(([collective]) => {
+          res.locals.collective = collective;
+          req.collective = collective;
+
+          next();
+        })
+        .catch(next);
+    },
+
+    index(req, res) {
+      res.render('collectives/index');
+    },
+
+    show(req, res) {
+      res.render('collectives/show');
+    },
+  },
+});
+
+module.exports = new CollectivesController();
--- seeds/collectives.js
+++ seeds/collectives.js
@@ -4,21 +4,48 @@ exports.seed = (knex) => {
   return knex('Collectives').del()
     .then(() => {
       const collectives = [
-        'For-Profit College Collective',
-        'Student Debt Collective',
-        'Credit Card Debt Collective',
-        'Mortgage Debt Collective',
-        'Payday Loans Collective',
-        'Auto Loans Collective',
-        'Court Fines and Fees Collective',
-        'Medical Debt Collective',
-        'Solidarity Bloc',
+        {
+          id: '11111111-1111-1111-1111-111111111111',
+          name: 'For-Profit College Collective',
+        },
+        {
+          id: '22222222-2222-2222-2222-222222222222',
+          name: 'Student Debt Collective',
+        },
+        {
+          id: '33333333-3333-3333-3333-333333333333',
+          name: 'Credit Card Debt Collective',
+        },
+        {
+          id: '44444444-4444-4444-4444-444444444444',
+          name: 'Mortgage Debt Collective',
+        },
+        {
+          id: '55555555-5555-5555-5555-555555555555',
+          name: 'Payday Loans Collective',
+        },
+        {
+          id: '66666666-6666-6666-6666-666666666666',
+          name: 'Auto Loans Collective',
+        },
+        {
+          id: '77777777-7777-7777-7777-777777777777',
+          name: 'Court Fines and Fees Collective',
+        },
+        {
+          id: '88888888-8888-8888-8888-888888888888',
+          name: 'Medical Debt Collective',
+        },
+        {
+          id: '99999999-9999-9999-9999-999999999999',
+          name: 'Solidarity Bloc',
+        },
       ];
 
       return knex('Collectives').insert(collectives.map((collective) => {
         return {
-          id: uuid.v4(),
-          name: collective,
+          id: collective.id,
+          name: collective.name,
           created_at: new Date(),
           updated_at: new Date(),
         };
