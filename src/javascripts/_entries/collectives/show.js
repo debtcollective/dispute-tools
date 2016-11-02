1b207e5174c5f80d767f6f5d73fd8a94c9980bf9
Noel Delgado <auhcsei@gmail.com>
2016-11-02 12:08:56 -0600

    add / sticky tabs

--- src/javascripts/_entries/collectives/show.js
+++ src/javascripts/_entries/collectives/show.js
@@ -1,6 +1,8 @@
+import debounce from 'lodash/debounce';
 import NodeSupport from '../../lib/widget/NodeSupport';
 import Common from '../../components/Common';
 import Tabs from '../../components/Tabs';
+import CollectiveStickyTabs from '../../components/collectives/StickyTabs';
 import Modal from '../../components/Modal';
 
 class ViewCollectivesShow extends NodeSupport {
@@ -20,24 +22,62 @@ class ViewCollectivesShow extends NodeSupport {
       defaultTab: 'panel-manifest',
     }));
 
+    this.appendChild(new CollectiveStickyTabs({
+      name: 'HandleStickyHeader',
+      element: document.querySelector('[role="tablist"]'),
+    }));
+
     this.disputeIds = config.disputeIds;
 
-    this._bindEvents();
+    this.tabsOffsetSection = document.querySelector('[data-offset-section]');
+    this.tabsOffset = 0;
+
+    this._setup()
+      ._bindEvents();
+  }
+
+  _setup() {
+    this.tabsOffset = this.tabsOffsetSection.offsetHeight;
+
+    return this;
   }
 
   _bindEvents() {
-    this.handlers = {};
+    this._resizeHandlerRef = this._resizeHandler.bind(this);
+    window.addEventListener('resize', debounce(this._resizeHandlerRef, 200));
 
+    this._tabsChangeHandlerRef = this._tabsChangeHandler.bind(this);
+    this.Tabs.bind('change', this._tabsChangeHandlerRef);
+
+    this.modalHandlers = {};
     this.disputeIds.forEach(id => {
       this.appendChild(new Modal({
         name: `modal-${id}`,
         element: document.querySelector(`[data-component-modal="tool-modal-${id}"]`),
       }));
 
-      this.handlers[id] = this._aboutClickHandler.bind(this, this[`modal-${id}`]);
+      this.modalHandlers[id] = this._aboutClickHandler.bind(this, this[`modal-${id}`]);
       document.getElementById(`tool-modal-toggler-${id}`)
-        .addEventListener('click', this.handlers[id]);
+        .addEventListener('click', this.modalHandlers[id]);
     });
+  }
+
+  _resizeHandler() {
+    this._setup();
+  }
+
+  /**
+   * Checks if the body scroll position needs to be updated after a new Tab has
+   * been activated. This is prevents displaying TabPanels on a different `y`
+   * position other than its very `top` caused because of the Tab’s `sticky`
+   * behaviour.
+   * @private
+   * @listens {Tabs.change}
+   */
+  _tabsChangeHandler() {
+    if (document.body.scrollTop > this.tabsOffset) {
+      document.body.scrollTop = this.tabsOffset;
+    }
   }
 
   _aboutClickHandler(instance) {
