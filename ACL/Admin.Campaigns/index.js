8f8379143436a6958f49b4bc5cdfeddae980ae9e
Sergio de la Garza <sergio@delagarza.io>
2016-11-10 12:37:30 -0600

    Admin.CampaignsController and ACL

---
+++ ACL/Admin.Campaigns/index.js
@@ -0,0 +1,33 @@
+/* globals Admin */
+module.exports = {
+  Visitor: [
+    ['new', 'create', 'edit', 'update', 'activate', 'deactivate', false],
+  ],
+  User: [
+    ['new', 'create', 'edit', 'update', 'activate', 'deactivate', false],
+  ],
+  CollectiveManager: [
+    ['new', 'create', 'edit', 'update', 'activate', 'deactivate', (req) => {
+      Admin.Campaign.query()
+        .where('id', req.params.id)
+        .then(([campaign]) => {
+          return Admin.Campaign.knex()
+            .table('CollectiveAdmins')
+            .where({
+              collective_id: campaign.collectiveId,
+              user_id: req.user.id,
+            });
+        })
+        .then((results) => {
+          if (results.length === 0) {
+            return false;
+          }
+
+          return true;
+        });
+    }],
+  ],
+  Admin: [
+    ['new', 'create', 'edit', 'update', 'activate', 'deactivate', true],
+  ],
+};
---
+++ controllers/Admin/CampaignsController.js
@@ -0,0 +1,128 @@
+/* globals neonode, CONFIG, Class, Admin, RestfulController, Collective, DisputeTool,
+NotFoundError */
+
+global.Admin = global.Admin || {};
+
+Class(Admin, 'Campaign').inherits(Campaign)({
+  resourceName: 'Admin.Campaigns',
+});
+
+Class(Admin, 'CampaignsController').inherits(RestfulController)({
+  beforeActions: [
+    // Authenticate
+    {
+      before(req, res, next) {
+        return neonode.controllers.Home._authenticate(req, res, next);
+      },
+      actions: ['new', 'create', 'edit', 'update', 'activate', 'deactivate'],
+    },
+    // Load Campaign
+    {
+      before: '_loadCampaign',
+      actions: ['edit', 'update', 'activate', 'deactivate'],
+    },
+    // Load Collectives
+    {
+      before(req, res, next) {
+        Admin.Collective.query()
+          .then((collectives) => {
+            return req.restifyACL(collectives);
+          })
+          .then((collectives) => {
+            req.collectives = collectives;
+            res.locals.collectives = collectives;
+
+            return next();
+          })
+          .catch(next);
+      },
+      actions: ['new', 'edit'],
+    },
+  ],
+
+  prototype: {
+    _loadCampaign(req, res, next) {
+      Admin.Campaign.query()
+        .where('id', req.params.id)
+        .include('collective')
+        .then((campaign) => {
+          if (campaign.length === 0) {
+            return next(new NotFoundError('Campaign not found'));
+          }
+
+          req.campaign = campaign[0];
+          res.locals.campaign = campaign[0];
+          return next();
+        })
+        .catch(next);
+    },
+
+    new(req, res) {
+      res.render('admin/campaigns/new');
+    },
+
+    create(req, res) {
+      const campaign = new Admin.Campaign(req.body);
+
+      campaign.active = false;
+
+      campaign.save()
+        .then(() => {
+          req.flash('success', 'The campaign has been created.');
+          res.redirect(CONFIG.router.helpers.Campaigns.show.url(campaign.id));
+        })
+        .catch((err) => {
+          res.status(400);
+
+          res.locals.errors = err.errors || err;
+
+          res.render('admin/campaigns/new');
+        });
+    },
+
+    edit(req, res) {
+      res.render('admin/campaigns/edit');
+    },
+
+    update(req, res) {
+      req.campaign.updateAttributes(req.body);
+
+      req.campaign.save()
+        .then(() => {
+          req.flash('success', 'The campaign has been updated.');
+          res.redirect(CONFIG.router.helpers.Campaigns.show.url(req.campaign.id));
+        })
+        .catch((err) => {
+          res.status(400);
+
+          res.locals.errors = err.errors || err;
+
+          res.render('admin/campaigns/edit');
+        });
+    },
+
+    activate(req, res, next) {
+      req.campaign.active = true;
+
+      req.campaign.save()
+        .then(() => {
+          req.flash('success', 'The campaign is now active.');
+          res.redirect(CONFIG.router.helpers.Campaigns.show.url(req.campaign.id));
+        })
+        .catch(next);
+    },
+
+    deactivate(req, res, next) {
+      req.campaign.active = false;
+
+      req.campaign.save()
+        .then(() => {
+          req.flash('success', 'The campaign is now inactive.');
+          res.redirect(CONFIG.router.helpers.Campaigns.show.url(req.campaign.id));
+        })
+        .catch(next);
+    },
+  },
+});
+
+module.exports = new Admin.CampaignsController();
