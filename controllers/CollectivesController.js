464d35dee9237aedadb7b43f698b64601fb2b751
Sergio de la Garza <sergio@delagarza.io>
2016-11-15 12:40:40 -0600

    Add user_count to collectives Fix Collectives#join to update user_count Fix Users#create to update collectives user_count

--- controllers/CollectivesController.js
+++ controllers/CollectivesController.js
@@ -95,11 +95,26 @@ const CollectivesController = Class('CollectivesController').inherits(RestfulCon
     },
 
     join(req, res, next) {
-      User.knex()
-        .table('UsersCollectives')
-        .insert({
-          user_id: req.user.id,
-          collective_id: req.collective.id,
+      const knex = User.knex();
+
+      Collective.query()
+        .transaction((trx) => {
+          return knex
+            .table('UsersCollectives')
+            .transacting(trx)
+            .insert({
+              user_id: req.user.id,
+              collective_id: req.collective.id,
+            })
+            .then(() => {
+              req.collective.userCount++;
+
+              return req.collective
+                .transacting(trx)
+                .save();
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
         })
         .then(() => {
           req.flash('success', `You have successfully joined ${req.collective.name}`);
