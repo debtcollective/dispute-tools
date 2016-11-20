99076fd7b11ba06e653b24a98f686a80d292eb69
Noel Delgado <auhcsei@gmail.com>
2016-11-19 19:29:42 -0600

    add / API endpoint
    - createCampaignPost

--- src/javascripts/lib/api.js
+++ src/javascripts/lib/api.js
@@ -22,12 +22,37 @@ export default {
    * @param {function} [callback] - the callback that handles the response.
    */
   updateDisputeData(args, callback) {
-    if (!args || !args.disputeId || !args) {
+    if (!args || !args.disputeId) {
       throw new Error('Missing required params');
     }
 
     superagent
       .put(`/disputes/${args.disputeId}/update-dispute-data`)
+      .send(args.body || {})
+      .set('X-CSRF-Token', csrfToken)
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        if (typeof callback === 'function') {
+          callback(err, res);
+        }
+      });
+  },
+
+  /**
+   * Creates a new Post related to a Campaign.
+   * @param {Object} args - the arguments needed to hit the endpoint.
+   * @param {string} args.campaignId - the campaign id to which the post will be related.
+   * @param {Object} [args.body={}] - the request body to be send to the server.
+   * @param {string} args.body.tye - one of ['Text', 'Image', 'Poll']
+   * @param {function} [callback] - the callback to handle the server response.
+   */
+  createCampaignPost(args, callback) {
+    if (!args || !args.campaignId) {
+      throw new Error('Missing required params');
+    }
+
+    superagent
+      .post(`/campaigns/${args.campaignId}/posts`)
       .send(args.body || {})
       .set('X-CSRF-Token', csrfToken)
       .set('Accept', 'application/json')
