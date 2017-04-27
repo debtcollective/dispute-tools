77f77cf3c5c854ec4935349fc44cc02ffb8a7c78
Alvaro Cabrera <pateketrueke@gmail.com>
2017-04-26 21:14:06 -0500

    Restore previous download() impl

--- controllers/DisputesController.js
+++ controllers/DisputesController.js
@@ -278,59 +278,17 @@ const DisputesController = Class('DisputesController').inherits(RestfulControlle
     },
 
     download(req, res, next) {
-      Promise.all([
-        DisputeRenderer.query()
-          .where({
-            dispute_id: req.params.id,
-          })
-          .limit(1),
-        Dispute.query().where('id', req.params.id),
-      ])
-        .then(([renderer, [dispute]]) => {
+      DisputeRenderer.query()
+        .where({
+          dispute_id: req.params.id,
+        })
+        .limit(1)
+        .then((renderer) => {
           if (renderer.length === 0) {
-            next(new NotFoundError('File not found'));
-            return;
+            return next(new NotFoundError('File not found'));
           }
 
-          const zipFile = path.join(process.cwd(), 'public', renderer[0].zip.url('original'));
-
-          const newZip = new AdmZip();
-
-          Object.keys(dispute.data.forms).forEach((form) => {
-            const data = [];
-
-            Object.keys(dispute.data.forms[form]).forEach((key) => {
-              data.push([key, dispute.data.forms[form][key]]);
-            });
-
-            newZip.addFile(`${form}.xlsx`, xlsx.build([{ name: form, data }]));
-          });
-
-          const info = [];
-
-          Object.keys(dispute.data).forEach((key) => {
-            if (typeof dispute.data[key] === 'string') {
-              info.push([key, dispute.data[key]]);
-            }
-
-            if (typeof dispute.data[key] === 'boolean') {
-              info.push([key, dispute.data[key] ? 'yes' : 'no']);
-            }
-          });
-
-          newZip.addFile('personal-information.xlsx',
-            xlsx.build([{ name: 'personal-information', data: info }]));
-
-          newZip.addLocalFile(zipFile);
-
-          res.setHeader('Content-type', 'application/zip');
-          res.setHeader('Content-disposition',
-            `attachment; filename=dispute-${req.params.id}.zip`);
-
-          const data = newZip.toBuffer();
-
-          res.setHeader('Content-Length', String(data.length));
-          res.end(data, 'binary');
+          return res.sendFile(path.join(process.cwd(), 'public', renderer[0].zip.url('original')));
         });
     },
 
