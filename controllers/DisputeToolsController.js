bdc54116111d0faf8ba98fdc2961195af9e846f2
Orlando Del Aguila <orlando@hashlabs.com>
2017-12-03 18:18:17 -0600

    adding excerpt to dispute tools and showing it in listing

--- controllers/DisputeToolsController.js
+++ controllers/DisputeToolsController.js
@@ -1,76 +1,81 @@
 /* globals RestfulController, DisputeTool, neonode, Class, CONFIG, Dispute */
 const marked = require('marked');
 
-const DisputeToolsController = Class('DisputeToolsController')
-  .inherits(RestfulController)({
-    beforeActions: [
-      {
-        before: [
-          (req, res, next) => {
-            return neonode.controllers.Home._authenticate(req, res, next);
-          },
-          (req, res, next) => {
-            DisputeTool.query()
-              .where({
-                id: req.params.id,
-              })
-              .then(([disputeTool]) => {
-                res.locals.disputeTool = disputeTool;
+const DisputeToolsController = Class('DisputeToolsController').inherits(
+  RestfulController
+)({
+  beforeActions: [
+    {
+      before: [
+        (req, res, next) => {
+          return neonode.controllers.Home._authenticate(req, res, next);
+        },
+        (req, res, next) => {
+          DisputeTool.query()
+            .where({
+              id: req.params.id,
+            })
+            .then(([disputeTool]) => {
+              res.locals.disputeTool = disputeTool;
 
-                next();
-              })
-              .catch(next);
-          },
-        ],
-        actions: ['show'],
-      },
-    ],
-    prototype: {
-      index(req, res, next) {
-        DisputeTool.query()
-          .orderBy('id', 'ASC')
-          .then((disputeTools) => {
-            res.locals.disputeTools = [];
+              next();
+            })
+            .catch(next);
+        },
+      ],
+      actions: ['show'],
+    },
+  ],
+  prototype: {
+    index(req, res, next) {
+      DisputeTool.query()
+        .orderBy('id', 'ASC')
+        .then(disputeTools => {
+          res.locals.disputeTools = [];
 
-            disputeTools.forEach((dispute) => {
-              res.locals.disputeTools.push({
-                id: dispute.id,
-                name: dispute.name,
-                about: marked(dispute.about),
-                completed: dispute.completed,
-                data: dispute.data,
-              });
+          disputeTools.forEach(disputeTool => {
+            res.locals.disputeTools.push({
+              id: disputeTool.id,
+              name: disputeTool.name,
+              excerpt: disputeTool.excerpt,
+              about: marked(disputeTool.about),
+              completed: disputeTool.completed,
+              data: disputeTool.data,
             });
-            res.render('dispute-tools/index');
-          })
-          .catch(next);
-      },
+          });
+          res.render('dispute-tools/index');
+        })
+        .catch(next);
+    },
 
-      show(req, res, next) {
-        const disputeTool = res.locals.disputeTool;
+    show(req, res, next) {
+      const disputeTool = res.locals.disputeTool;
 
-        Object.keys(disputeTool.data.options).forEach(option => {
-          const _option = disputeTool.data.options[option];
+      Object.keys(disputeTool.data.options).forEach(option => {
+        const _option = disputeTool.data.options[option];
 
-          if (_option.more) {
-            _option.more = marked(_option.more);
-          }
-        });
+        if (_option.more) {
+          _option.more = marked(_option.more);
+        }
+      });
 
-        if (Object.keys(disputeTool.data.options).length === 1) {
-          return disputeTool.createDispute({
+      if (Object.keys(disputeTool.data.options).length === 1) {
+        return disputeTool
+          .createDispute({
             option: 'none',
             user: req.user,
           })
-          .then((disputeId) => {
-            return res.redirect(CONFIG.router.helpers.Disputes.show.url(disputeId));
+          .then(disputeId => {
+            return res.redirect(
+              CONFIG.router.helpers.Disputes.show.url(disputeId)
+            );
           })
           .catch(next);
-        }
+      }
 
-        return res.render('dispute-tools/show');
-      },
+      return res.render('dispute-tools/show');
     },
-  });
+  },
+});
 
 module.exports = new DisputeToolsController();
--- seeds/20171113114842_dispute_tools.js
+++ seeds/20171113114842_dispute_tools.js
@@ -11,42 +11,28 @@ const personalInformationFieldSets = [
           name: 'name',
           label: 'Your Name',
           columnClassName: 'md-col-8',
-          validations: [
-            'required',
-            'maxLength:128',
-          ],
+          validations: ['required', 'maxLength:128'],
         },
         {
           name: 'ssn',
           label: 'Social Security Number',
           placeholder: 'AAA-GG-SSSS',
           columnClassName: 'md-col-4',
-          validations: [
-            'required',
-            'alphaDash',
-            'minLength:9',
-            'maxLength:11',
-          ],
+          validations: ['required', 'alphaDash', 'minLength:9', 'maxLength:11'],
         },
       ],
       [
         {
           name: 'address1',
           label: 'Your Mailing Address',
-          validations: [
-            'required',
-            'maxLength:128',
-          ],
+          validations: ['required', 'maxLength:128'],
         },
       ],
       [
         {
           name: 'address2',
           label: 'City, State, Zip Code',
-          validations: [
-            'required',
-            'maxLength:128',
-          ],
+          validations: ['required', 'maxLength:128'],
         },
       ],
       [
@@ -55,21 +41,14 @@ const personalInformationFieldSets = [
           label: 'Your email',
           placeholder: 'you@example.com',
           columnClassName: 'md-col-6',
-          validations: [
-            'required',
-            'email',
-            'maxLength:128',
-          ],
+          validations: ['required', 'email', 'maxLength:128'],
         },
         {
           name: 'phone',
           label: 'Your telephone',
           placeholder: '(555) 555-5555',
           columnClassName: 'md-col-6',
-          validations: [
-            'required',
-            'maxLength:20',
-          ],
+          validations: ['required', 'maxLength:20'],
         },
       ],
     ],
@@ -82,10 +61,7 @@ const personalInformationFieldSets = [
           name: 'schoolName',
           label: 'Name of the school where you incurred the debt',
           columnClassName: 'md-col-8',
-          validations: [
-            'required',
-            'maxLength:128',
-          ],
+          validations: ['required', 'maxLength:128'],
         },
       ],
     ],
@@ -94,26 +70,21 @@ const personalInformationFieldSets = [
     title: 'FFEL Loan',
     yesno: true,
     yesNoLabel: 'Are you a FFEL holder?',
-    yesNoCaption: 'If you have FFEL loans, you will need to provide the name and address of the originator of your loan. If you don’t know the name of the guarantor, you can call the Department of the Treasury and ask them for the name and address of your guarantor. Their number is: <span class="-white">1-800-304-3107</span>.',
+    yesNoCaption:
+      'If you have FFEL loans, you will need to provide the name and address of the originator of your loan. If you don’t know the name of the guarantor, you can call the Department of the Treasury and ask them for the name and address of your guarantor. Their number is: <span class="-white">1-800-304-3107</span>.',
     fields: [
       [
         {
           name: 'guarantyAgency',
           label: 'Name of Guaranty Agency',
           columnClassName: 'md-col-6',
-          validations: [
-            'required',
-            'maxLength:128',
-          ],
+          validations: ['required', 'maxLength:128'],
         },
         {
           name: 'guarantyAgencyMailingAddress',
           label: 'Guaranty Agency mailing address',
           columnClassName: 'md-col-6',
-          validations: [
-            'required',
-            'maxLength:128',
-          ],
+          validations: ['required', 'maxLength:128'],
         },
       ],
     ],
@@ -128,10 +99,7 @@ const personalInformationFieldSets = [
           name: 'employer',
           label: 'Current Employer',
           columnClassName: 'md-col-8',
-          validations: [
-            'required',
-            'maxLength:128',
-          ],
+          validations: ['required', 'maxLength:128'],
         },
         {
           name: 'employmentDate',
@@ -139,10 +107,7 @@ const personalInformationFieldSets = [
           label: 'Beggining Date',
           placeholder: 'mm-dd-yyyy',
           columnClassName: 'md-col-4',
-          validations: [
-            'required',
-            'maxLength:20',
-          ],
+          validations: ['required', 'maxLength:20'],
         },
       ],
       [
@@ -150,29 +115,20 @@ const personalInformationFieldSets = [
           name: 'employerAddress1',
           label: 'Employer Mailing Address 1',
           columnClassName: 'md-col-8',
-          validations: [
-            'required',
-            'maxLength:128',
-          ],
+          validations: ['required', 'maxLength:128'],
         },
         {
           name: 'employerAddress2',
           label: 'Employer City, State, Zip Code',
           columnClassName: 'md-col-8',
-          validations: [
-            'required',
-            'maxLength:128',
-          ],
+          validations: ['required', 'maxLength:128'],
         },
         {
           name: 'employerPhone',
           label: 'Employer Phone',
           placeholder: '(555) 555-5555',
           columnClassName: 'md-col-4',
-          validations: [
-            'required',
-            'maxLength:20',
-          ],
+          validations: ['required', 'maxLength:20'],
         },
       ],
     ],
@@ -195,7 +151,8 @@ const personalStatement = {
   mimeTypes: ['image/jpeg', 'image/png', 'application/pdf'],
   maxFileSize: 5242880,
   title: 'Personal Statement',
-  description: 'In addition to providing evidence against the school, you can write a personal statement describing how your school lied to and defrauded you and upload it here.',
+  description:
+    'In addition to providing evidence against the school, you can write a personal statement describing how your school lied to and defrauded you and upload it here.',
   uploadButtonText: 'Upload file',
   footerNotes: 'JPEG, PNG, PDF format',
 };
@@ -213,13 +170,17 @@ const evidenceUploader = {
   footerNotes: 'JPEG, PNG, PDF format',
 };
 
-exports.seed = (knex) => knex('DisputeTools').del()
-  .then(() => {
-    const tools = [
-      {
-        id: '11111111-1111-1111-1111-111111111111',
-        name: 'Wage Garnishment Dispute',
-        about: `### Wage Garnishment for Federal Student Loans Dispute
+exports.seed = knex =>
+  knex('DisputeTools')
+    .del()
+    .then(() => {
+      const tools = [
+        {
+          id: '11111111-1111-1111-1111-111111111111',
+          name: 'Dispute Your Wages Being Taken',
+          excerpt:
+            'Use this tool if you have a federal student loan that is being garnished or if you have been threatened with garnishment.',
+          about: `### Dispute Your Wages Being Taken
 
 If your wages are being garnished or if you received a letter threatening wage garnishment, you have a lot in common with thousands of other people whose wages are seized every year. This is wrong, since no one should have to go into debt for education!
 
@@ -229,12 +190,14 @@ PLEASE NOTE: **Before you begin the wage garnishment dispute process, you should
 
 If you have Direct loans, we will send your dispute to the Department of Education. If you have FFEL loans, you will need to provide the name and address of the originator of your loan. If you don't know the name of the guarantor, you can call the Department of the Treasury and ask them for the name and address of your guarantor.<br/>
 The number is: 1-800-304-3107.`,
-        completed: 0,
-      },
-      {
-        id: '11111111-1111-2222-1111-111111111111',
-        name: 'Tax Offset Dispute',
-        about: `### Tax Offset for Federal Student Loans
+          completed: 0,
+        },
+        {
+          id: '11111111-1111-2222-1111-111111111111',
+          name: 'Dispute Your Tax Return Being Taken',
+          excerpt:
+            'Use this tool if your tax tax return is being seized for an unpaid federal student loan.',
+          about: `### Dispute Your Tax Return Being Taken
 
 If your taxes are being seized ("offset") or if you have been threatened with offset, you have a lot in common with thousands of other people whose taxes are offset each year. This is wrong, since no one should have to go into debt for education!
 
@@ -243,12 +206,14 @@ You can use this form to submit a dispute to the Department of Education. The De
 **What if I suspect that my taxes will be offset but I haven't received a notice?**
 
 The Department of Education usually sends offset notices once per year in the summer. If you want to know whether your taxes will be offset as a result of a federal student loan, you can call the Bureau of the Fiscal Service at 1-800-304-3107. Making this call does not make it more likely that your taxes will be offset.`,
-        completed: 0,
-      },
-      {
-        id: '11111111-1111-3333-1111-111111111111',
-        name: 'General Debt Dispute Letter',
-        about: `### General Debt Dispute Letter
+          completed: 0,
+        },
+        {
+          id: '11111111-1111-3333-1111-111111111111',
+          name: 'Dispute Any Debt in Collections',
+          excerpt:
+            'Use this tool to dispute any debt (except a student loan) that has been sent to collections. Make them prove it!',
+          about: `### Dispute Any Debt in Collections
 
 #### For any defaulted personal debt
 
@@ -257,39 +222,44 @@ Debt collectors are shady. They often try to collect on debts that they do not h
 Before you begin to use this tool, you should make sure you have the name and address of the collections agency that is attempting to collect from you.
 
 **If you don't have the name and address of the collection agency, you will not be able to file this dispute.**`,
-        completed: 0,
-      },
+          completed: 0,
+        },
 
-      {
-        id: '11111111-1111-4444-1111-111111111111',
-        name: 'Credit Report Dispute Letter',
-        about: `### Credit Report Dispute Letter
+        {
+          id: '11111111-1111-4444-1111-111111111111',
+          name: 'Dispute Errors on Your Credit Report',
+          excerpt:
+            'Most of us have been victims of bad credit reporting. Use this tool to send a dispute.',
+          about: `### Dispute Errors on Your Credit Report
 
 Millions of us have errors on our credit reports, which makes it harder to do basic things like get a job or rent an apartment. Fight back! You can get a free copy of your credit report once per year at <a href="http://annualcreditreport.com" target="_blank" rel="noopener noreferrer">annualcreditreport.com</a> or call 1-877- 322-8228. For a list of common errors to look for, go <a href="http://www.consumerfinance.gov/askcfpb/1339/if-credit-reporting-error-corrected-how-long-will-it-take-i-find-out-results.html" target="_blank" rel="noopener noreferrer">here</a>. Once you have determined that there are errors on your report, you can use this tool to ask for a correction. This tool will help you write a dispute letter to the three main credit reporting agencies. The Debt Collective will submit the letters your behalf. For more information about the process, go <a href="http://www.consumerfinance.gov/askcfpb/1261/what-are-errors-show-credit-reports-out-having-creditors-report-your-accounts-credit-bureaus.html" target="_blank" rel="noopener noreferrer">here</a>. Please have a photo of your picture ID ready to upload (taking a picture with your camera phone is fine).
 `,
-        completed: 0,
-      },
+          completed: 0,
+        },
 
-      //         {
-      //           id: '11111111-1111-5555-1111-111111111111',
-      //           name: 'Defense to Repayment application',
-      //           about: `### Defense to Repayment application
-      //
-      // If your taxes are being seized ("offset") or if you have been threatened with offset, you have a lot in common with thousands of other people whose taxes are offset each year. This is wrong, since no one should have to go into debt for education!
-      //
-      // You can use this form to submit a dispute to the Department of Education. The Department's form is unnecessarily complicated. We have streamlined the process for our members. You can make sure your offset is placed on hold if you file this dispute by 65 days after the date you received the offset notice. You can also stop an offset that has already begun if you receive a favorable decision from the Department.
-      //
-      // What if I suspect that my taxes will be offset but I haven't received a notice?
-      //
-      // The Department of Education usually sends offset notices once per year in the summer. If you want to know whether your taxes will be offset as a result of a federal student loan, you can call the Bureau of the Fiscal Service at 1-800-304-3107. Making this call does not make it more likely that your taxes will be offset.
-      // `,
-      //           completed: 0,
-      //         },
+        //         {
+        //           id: '11111111-1111-5555-1111-111111111111',
+        //           name: 'Defense to Repayment for Federal Student Loans',
+        //           excerpt: 'Most of us have been victims of bad credit reporting. Use this tool to send a dispute.',
+        //           about: `### Defense to Repayment for Federal Student Loans
+        //
+        // If your taxes are being seized ("offset") or if you have been threatened with offset, you have a lot in common with thousands of other people whose taxes are offset each year. This is wrong, since no one should have to go into debt for education!
+        //
+        // You can use this form to submit a dispute to the Department of Education. The Department's form is unnecessarily complicated. We have streamlined the process for our members. You can make sure your offset is placed on hold if you file this dispute by 65 days after the date you received the offset notice. You can also stop an offset that has already begun if you receive a favorable decision from the Department.
+        //
+        // What if I suspect that my taxes will be offset but I haven't received a notice?
+        //
+        // The Department of Education usually sends offset notices once per year in the summer. If you want to know whether your taxes will be offset as a result of a federal student loan, you can call the Bureau of the Fiscal Service at 1-800-304-3107. Making this call does not make it more likely that your taxes will be offset.
+        // `,
+        //           completed: 0,
+        //         },
 
-      {
-        id: '11111111-1111-6666-1111-111111111111',
-        name: 'Private Student Loan Dispute Letter',
-        about: `### Private Student Loan Dispute Letter
+        {
+          id: '11111111-1111-6666-1111-111111111111',
+          name: 'Dispute an Unpaid Private Student Loan',
+          excerpt:
+            'Use this tool to send a dispute to a private student loan in collections.',
+          about: `### Dispute an Unpaid Private Student Loan
 
 #### For any defaulted private student loan debt that has been sent to collections.
 
@@ -304,16 +274,18 @@ Before you begin to use this tool, you should make sure you have the name and ad
 If you don't have the name and address of the collection agency, you will not be able to file this dispute.
 
 `,
-        completed: 0,
-      },
-    ];
+          completed: 0,
+        },
+      ];
 
-    return knex('DisputeTools').insert(tools.map((tool) => ({
-      id: tool.id,
-      name: tool.name,
-      about: tool.about,
-      completed: tool.completed,
-      created_at: new Date(),
-      updated_at: new Date(),
-    })));
-  });
+      return knex('DisputeTools').insert(
+        tools.map(tool => ({
+          id: tool.id,
+          name: tool.name,
+          about: tool.about,
+          completed: tool.completed,
+          created_at: new Date(),
+          updated_at: new Date(),
+        }))
+      );
+    });
