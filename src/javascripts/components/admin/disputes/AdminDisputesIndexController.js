99c51e5db5da9a4b80209a335d62a3e30f1ee9ce
Orlando Del Aguila <orlando@hashlabs.com>
2017-12-17 15:11:48 -0600

    standardizing dispute show view

--- src/javascripts/components/admin/disputes/AdminDisputesIndexController.js
+++ src/javascripts/components/admin/disputes/AdminDisputesIndexController.js
@@ -4,7 +4,7 @@ import AdminDisputesIndexTableControls from './AdminDisputesIndexTableControls';
 import AdminDisputesIndexTable from './AdminDisputesIndexTable';
 import Modal from '../../Modal';
 import AdminDisputesAddStatusForm from './AdminDisputesAddStatusForm';
-import AdminShowDisputePanel from './AdminShowDisputePanel';
+import adminShowDisputePanel from './AdminShowDisputePanel';
 import AssignedToMeButton from './AssignedToMeButton';
 import mountManageDisputeAdmins from './ManageDisputeAdmins';
 
@@ -12,38 +12,52 @@ export default class AdminDisputesIndexController extends Widget {
   constructor(config) {
     super(config);
 
-    this.appendChild(new AdminDisputesIndexTableControls({
-      name: 'AdminDisputesIndexTableControls',
-      element: document.querySelector('thead'),
-    }));
+    this.appendChild(
+      new AdminDisputesIndexTableControls({
+        name: 'AdminDisputesIndexTableControls',
+        element: document.querySelector('thead'),
+      })
+    );
 
-    this.appendChild(new AdminDisputesIndexTable({
-      name: 'AdminDisputesIndexTable',
-      element: document.querySelector('tbody'),
-      disputes: this.disputes,
-    }));
+    this.appendChild(
+      new AdminDisputesIndexTable({
+        name: 'AdminDisputesIndexTable',
+        element: document.querySelector('tbody'),
+        disputes: this.disputes,
+      })
+    );
 
-    this.appendChild(new Modal({
-      name: 'addStatusModal',
-      element: document.querySelector('[data-component-modal="add-status"]'),
-    }));
+    this.appendChild(
+      new Modal({
+        name: 'addStatusModal',
+        element: document.querySelector('[data-component-modal="add-status"]'),
+      })
+    );
 
-    this.appendChild(new Modal({
-      name: 'viewDisputeModal',
-      element: document.querySelector('[data-component-modal="show-dispute"]'),
-    }));
+    this.appendChild(
+      new Modal({
+        name: 'viewDisputeModal',
+        element: document.querySelector(
+          '[data-component-modal="show-dispute"]'
+        ),
+      })
+    );
 
-    this.appendChild(new AdminShowDisputePanel({
-      name: 'AdminShowDisputePanel',
-      element: document.querySelector('[data-component-form="show-dispute"]'),
-    }));
+    // Dispute Show Modal
+    this.adminShowDisputePanel = adminShowDisputePanel();
 
-    this.appendChild(new AdminDisputesAddStatusForm({
-      name: 'AdminDisputesAddStatusForm',
-      element: document.querySelector('[data-component-form="dispute-add-status"]'),
-    }));
+    this.appendChild(
+      new AdminDisputesAddStatusForm({
+        name: 'AdminDisputesAddStatusForm',
+        element: document.querySelector(
+          '[data-component-form="dispute-add-status"]'
+        ),
+      })
+    );
 
-    const searchAdminId = /admin_id\]=([\d\w-]*)/.exec(decodeURIComponent(window.location.search));
+    const searchAdminId = /admin_id\]=([\d\w-]*)/.exec(
+      decodeURIComponent(window.location.search)
+    );
 
     this.originalQuery = {
       filters: {
@@ -60,14 +74,19 @@ export default class AdminDisputesIndexController extends Widget {
     this._query = JSON.parse(JSON.stringify(this.originalQuery));
 
     // Not sure why this button isn't within the scope of `this.element` but this works anyway
-    this.assignedToMeButtonContainer = document.querySelector('#assigned-to-me-button');
+    this.assignedToMeButtonContainer = document.querySelector(
+      '#assigned-to-me-button'
+    );
     // Has to happen after this._query is populated
     this.assignedToMeButton = new AssignedToMeButton({
       queryReference: this._query,
       adminId: this.assignedToMeButtonContainer.dataset.currentUserId,
-      applyFilters: () => this.AdminDisputesIndexTableControls.dispatch('applyFilters'),
+      applyFilters: () =>
+        this.AdminDisputesIndexTableControls.dispatch('applyFilters'),
     });
-    this.assignedToMeButtonContainer.appendChild(this.assignedToMeButton.element);
+    this.assignedToMeButtonContainer.appendChild(
+      this.assignedToMeButton.element
+    );
 
     this.pagination = document.querySelector('.Pagination ul');
 
@@ -111,16 +130,18 @@ export default class AdminDisputesIndexController extends Widget {
 
     this.AdminDisputesIndexTable.bind('addStatus', data => {
       this.AdminDisputesAddStatusForm.updateData(data.dispute);
+
       if (this.manageDisputeAdmins) {
         this.manageDisputeAdmins.setDisputeId(data.dispute.id);
       } else {
         this.manageDisputeAdmins = mountManageDisputeAdmins(data.dispute.id);
       }
+
       this.addStatusModal.activate();
     });
 
     this.AdminDisputesIndexTable.bind('show', data => {
-      this.AdminShowDisputePanel.updateData(data.dispute);
+      this.adminShowDisputePanel.dispute = data.dispute;
       this.viewDisputeModal.activate();
     });
 
@@ -140,11 +161,12 @@ export default class AdminDisputesIndexController extends Widget {
 
   _enableButtons() {
     if (
-      (this._query.name !== this.originalQuery.name) ||
-      (this._query.status !== this.originalQuery.status) ||
-      (this._query.filters.dispute_tool_id !== this.originalQuery.filters.dispute_tool_id) ||
-      (this._query.order !== this.originalQuery.order) ||
-      (this._query.filters.readable_id !== this.originalQuery.filters.readable_id)
+      this._query.name !== this.originalQuery.name ||
+      this._query.status !== this.originalQuery.status ||
+      this._query.filters.dispute_tool_id !==
+        this.originalQuery.filters.dispute_tool_id ||
+      this._query.order !== this.originalQuery.order ||
+      this._query.filters.readable_id !== this.originalQuery.filters.readable_id
     ) {
       return this.AdminDisputesIndexTableControls.enableApplyButton();
     }
