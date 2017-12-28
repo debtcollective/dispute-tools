ad097e5211d921bacd414bf0c04350bf1ad30af3
Shoshana Berleant <sberleant@gmail.com>
2017-12-28 11:28:24 -0800

    refactor test runners

--- tests/integration.js
+++ tests/integration.js
@@ -1,40 +1,6 @@
-/* globals logger */
-
-const glob = require('glob');
-const Mocha = require('mocha');
 const path = require('path');
+const testRunner = require('./runner');
 
-const mocha = new Mocha();
-mocha.reporter('spec');
-mocha.timeout(5000);
+const TESTDIR = path.join(process.cwd(), 'tests', 'integration');
 
-require(path.join(process.cwd(), '/bin/server.js'));
-
-const testDir = path.join(process.cwd(), 'tests', 'integration');
-
-const globOptions = {
-  cwd: testDir, // search the integration directory
-  matchBase: true }; // search entire tree
-
-glob.sync('*.js', globOptions)
-  .filter((filePath) => {
-    const fileName = path.parse(filePath).base;
-    return (fileName.indexOf(process.argv[2]) !== -1);
-  })
-  .forEach((file) => {
-    mocha.addFile(path.join(testDir, file));
-  });
-
-// run Mocha
-mocha.run((failures) => {
-  process.on('exit', () => {
-    process.exit(failures);
-  });
-  process.exit();
-});
-
-
-process.on('error', (err) => {
-  logger.error(err, err.stack);
-  process.exit(1);
-});
+testRunner(TESTDIR);
--- tests/unit.js
+++ tests/unit.js
@@ -1,33 +1,6 @@
-/* globals logger */
-
-const glob = require('glob');
-const Mocha = require('mocha');
 const path = require('path');
+const testRunner = require('./runner');
 
-const mocha = new Mocha();
-mocha.reporter('spec');
+const TESTDIR = path.join(process.cwd(), 'tests', 'unit');
 
-require(path.join(process.cwd(), '/bin/server.js'));
-
-glob.sync('tests/unit/**/*.js')
-  .filter((filePath) => {
-    const fileName = path.parse(filePath).base;
-
-    return (fileName.indexOf(process.argv[2]) !== -1);
-  })
-  .forEach((file) => {
-    mocha.addFile(path.join(process.cwd(), file));
-  });
-
-// run Mocha
-mocha.run((failures) => {
-  process.on('exit', () => {
-    process.exit(failures);
-  });
-  process.exit();
-});
-
-process.on('error', (err) => {
-  logger.error(err, err.stack);
-  process.exit(1);
-});
+testRunner(TESTDIR);
