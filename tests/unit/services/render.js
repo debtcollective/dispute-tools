67d0b9521a64dc687750cc8123d57a75ea07851f
Shoshana Berleant <sberleant@gmail.com>
2018-01-22 23:15:11 -0800

    wage garnishment pdf render

--- tests/unit/services/render.js
+++ tests/unit/services/render.js
@@ -1,28 +1,28 @@
-/* eslint-disable max-len */
-
+const pdftk = require('node-pdftk');
 const { expect } = require('chai');
 const { render } = require('../../../services/render');
 const {
   generalDebtDispute,
-  studentDebtDispute,
   privateStudentLoanDispute,
   creditReportDispute,
-  taxOffsetReview_a,
-  taxOffsetReview_b,
-  taxOffsetReview_c,
-  taxOffsetReview_d,
-  taxOffsetReview_e,
+  taxOffsetReviews,
+  wageGarnishmentDisputes,
 } = require('../../utils/sampleDisputeData');
 
-function basicRenderTest(sampleData, docTemplates = [1], timeout = 5000) {
+function basicRenderTest(sampleData, docTemplates = [1]) {
   return async function theTest() {
-    this.timeout(timeout);
     const files = await render(sampleData);
     expect(files.length).to.eq(docTemplates.length);
-    files.forEach((doc, i) => {
-      expect(doc.length).to.eq(docTemplates[i]);
-      doc.forEach(template => expect(template.rendered).to.be.defined);
-    });
+    return Promise.all(
+      files.map(async (doc, i) => {
+        expect(doc.length).to.eq(docTemplates[i]);
+        doc.forEach(template => expect(template.rendered).to.be.defined);
+        return pdftk
+          .input(doc[0].rendered)
+          .generateFdf()
+          .output();
+      }),
+    );
   };
 }
 
@@ -42,32 +42,132 @@ describe('render', () => {
   });
 
   describe('pdf', () => {
+    const personalInfoKeys = ['city', 'name', 'state', 'address1', 'zip-code'];
+    const employmentInfoKeys = ['er', 'erCity', 'erPhone', 'erState', 'mentDate', 'erZipCode', 'erAddress1'];
+    const schoolInfoKeys = ['Name', '-city', '-state', '-address', '-zip-code'];
+    // prettier-ignore
+    const supplementalKeys = ['program-of-study', 'option5-text', 'tuition-payment', 'entrance-exam-supporter-zip-code',
+      'entrance-exam-supporter-name', 'entrance-exam-supporter-phone', 'entrance-exam-supporter-city', 'student-name',
+      'entrance-exam-supporter-state', 'entrance-exam-improper-explain', 'entrance-exam-supporter-address', 'law'];
+
+    const expectObjectionNum = (fdf, num) => expect(fdf.includes(num), `the objection number ${num} is not marked correctly`).true
+    
+    const expectContains = (fdf, testData, keys, prefix = '') => {
+      const formData = testData.data.forms['personal-information-form'];
+      keys.forEach(key => {
+        const pkey = prefix + key;
+        if (formData[pkey]) {
+          expect(fdf.includes(formData[pkey]), `generated pdf does not contain ${pkey} (${formData[pkey]})`).true;
+        }
+      });
+    };
+
+    const expectCorrectLoanDischargeApplication = (fdf, form, supplementalPrefix) => {
+      expectContains(fdf, form, personalInfoKeys);
+      expectContains(fdf, form, schoolInfoKeys, 'school');
+      expectContains(fdf, form, supplementalKeys, supplementalPrefix);
+    };
+
     describe('tax offset review', () => {
-      it('a: should render the pdf', basicRenderTest(taxOffsetReview_a));
+      it('a: should render the pdf', async () => {
+        const fdfs = await basicRenderTest(taxOffsetReviews.A, [1])();
 
-      it('b: should render the pdf', basicRenderTest(taxOffsetReview_b));
+        expectContains(fdfs[0], taxOffsetReviews.A, personalInfoKeys.concat(['ssn']));
+      });
 
-      it(
-        'c: should render the pdfs',
-        basicRenderTest(taxOffsetReview_c, [1, 1]),
-      );
+      it('b: should render the pdf', async () => {
+        const fdfs = await basicRenderTest(taxOffsetReviews.B, [1])();
 
-      it(
-        'd: should render the pdfs',
-        basicRenderTest(taxOffsetReview_d, [1, 1]),
-      );
+        expectContains(fdfs[0], taxOffsetReviews.B, personalInfoKeys.concat(['ssn']));
 
-      it(
-        'e: should render the pdfs',
-        basicRenderTest(taxOffsetReview_e, [1, 1]),
-      );
+        // because dispute option is INPERSON
+        expectContains(fdfs[0], taxOffsetReviews.B, ['phone']);
+        expectObjectionNum(fdfs[0], '7')
+      });
+
+      it.skip('c: should render the pdf', async () => {
+        // https://gitlab.com/debtcollective/debtcollective/issues/308
+        const fdfs = await basicRenderTest(taxOffsetReviews.C, [1, 1])();
+
+        expectContains(fdfs[0], taxOffsetReviews.C, personalInfoKeys.concat(['schoolName', 'ssn']));
+        expectCorrectLoanDischargeApplication(fdfs[1], taxOffsetReviews.C, 'atb-');
+      });
+
+      it('d: should render the pdf', async () => {
+        const fdfs = await basicRenderTest(taxOffsetReviews.D, [1, 1])();
+
+        expectContains(fdfs[0], taxOffsetReviews.D, personalInfoKeys.concat(['schoolName', 'ssn']));
+
+        // because dispute option is BYPHONE
+        expectContains(fdfs[0], taxOffsetReviews.D, ['phone']);
+        expectObjectionNum(fdfs[0], '11')
+
+        expectCorrectLoanDischargeApplication(fdfs[1], taxOffsetReviews.D, 'atbd-');
+      });
+
+      it.skip('e: should render the pdf', async () => {
+        // https://gitlab.com/debtcollective/debtcollective/issues/308
+        const fdfs = await basicRenderTest(taxOffsetReviews.E, [1, 1])();
+
+        expectContains(fdfs[0], taxOffsetReviews.E, personalInfoKeys.concat(['schoolName', 'ssn']));
+
+        // because dispute option is INPERSON
+        expectContains(fdfs[0], taxOffsetReviews.E, ['phone']);
+        expectObjectionNum(fdfs[0], '12')
+
+        expectCorrectLoanDischargeApplication(fdfs[1], taxOffsetReviews.E, 'fc-');
+      });
     });
-  });
 
-  describe('graphicsmagick', async () => {
-    it(
-      'should render the pdf',
-      basicRenderTest(studentDebtDispute, [6], 10000),
-    );
+    describe('wage garnishment dispute', () => {
+      const expectCorrectForm = (fdf, form) => {
+        expectContains(fdf, form, personalInfoKeys.concat(['ssn']));
+        expectContains(fdf, form, employmentInfoKeys, 'employ');
+      };
+
+      it('a: should render the pdf', async () => {
+        const fdfs = await basicRenderTest(wageGarnishmentDisputes.A, [1])();
+
+        expectCorrectForm(fdfs[0], wageGarnishmentDisputes.A);
+      });
+
+      it('b: should render the pdf', async () => {
+        const fdfs = await basicRenderTest(wageGarnishmentDisputes.B, [1])();
+
+        expectCorrectForm(fdfs[0], wageGarnishmentDisputes.B);
+
+        // because dispute option is INPERSON
+        expectObjectionNum(fdfs[0], '10')
+        expect(fdfs[0].includes('X'), `disputeProcessCity is not selected`).true;
+      });
+
+      it('c: should render the pdf', async () => {
+        const fdfs = await basicRenderTest(wageGarnishmentDisputes.C, [1, 1])();
+
+        expectCorrectForm(fdfs[0], wageGarnishmentDisputes.C);
+      });
+
+      it('d: should render the pdf', async () => {
+        const fdfs = await basicRenderTest(wageGarnishmentDisputes.D, [1, 1])();
+
+        expectCorrectForm(fdfs[0], wageGarnishmentDisputes.D);
+        expectContains(fdfs[0], wageGarnishmentDisputes.D, ['schoolName']);
+
+        // because dispute option is BYPHONE
+        expectContains(fdfs[0], wageGarnishmentDisputes.D, ['phone']);
+        expectObjectionNum(fdfs[0], '12')
+      });
+
+      it('e: should render the pdf', async () => {
+        const fdfs = await basicRenderTest(wageGarnishmentDisputes.E, [1, 1])();
+
+        expectCorrectForm(fdfs[0], wageGarnishmentDisputes.E);
+        expectContains(fdfs[0], wageGarnishmentDisputes.E, ['schoolName']);
+
+        // because dispute option is INPERSON
+        expectObjectionNum(fdfs[0], '14')
+        expect(fdfs[0].includes('X'), `disputeProcessCity is not selected`).true;
+      });
+    });
   });
 });
