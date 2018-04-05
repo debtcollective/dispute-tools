304607bb6b334a6a486474d8d4d880346dd8b78d
Alexandre Marcondes <alex@marcondesian.net>
2018-04-05 18:45:55 -0400

    Use better folder structure and file names

--- migrations/20180329184303_add_unique_constraint_dispute_admins.js
+++ migrations/20180329184303_add_unique_constraint_dispute_admins.js
@@ -1,9 +1,7 @@
-exports.up = knex => {
-  knex.raw(
+exports.up = knex => knex.schema.raw(
     'alter table "AdminsDisputes" add constraint dispute_id_admin_id_unique_constraint unique (admin_id, dispute_id)',
   );
-};
 
-exports.down = knex => {
-  knex.raw('alter table "AdminsDisputes" drop constraint dispute_id_admin_id_unique_constraint');
-};
+exports.down = knex => knex.schema.raw(
+    'alter table "AdminsDisputes" drop constraint dispute_id_admin_id_unique_constraint',
+  );
