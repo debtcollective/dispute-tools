2665c0c035e6012101ebdac56e4cb8b41d59c7b9
Alexandre Marcondes <alex@marcondesian.net>
2018-04-08 12:03:12 -0400

    Add oneOf Checkit validation extension

--- shared/Checkit.js
+++ shared/Checkit.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const Checkit = require('checkit');
 
 Checkit.Validator.prototype.ssn = function ssn(val) {
@@ -5,9 +6,27 @@ Checkit.Validator.prototype.ssn = function ssn(val) {
   return matched !== null && matched.length === 9;
 };
 
+// eslint-disable-next-line no-confusing-arrow
+const getOptions = (options, caseSensitive) =>
+  caseSensitive !== 'false'
+    ? options.split(',').map(_.trim)
+    : options
+        .toLowerCase()
+        .split(',')
+        .map(_.trim);
+
+Checkit.Validator.prototype.oneOf = function oneOf(val, options, caseSensitive) {
+  return Array.isArray(val)
+    ? _.every(val, v => Checkit.Validator.prototype.oneOf(v, options, caseSensitive))
+    : getOptions(options, caseSensitive).includes(
+        _.trim(caseSensitive !== 'false' ? val : val.toLowerCase()),
+      );
+};
+
 module.exports = class extends Checkit {
   constructor(...args) {
     super(...args);
     this.messages.ssn = 'Invalid social security number';
+    this.messages.oneOf = 'Invalid option. Was not one of {{var_1}}';
   }
 };
---
+++ tests/unit/shared/CheckitTests.js
@@ -0,0 +1,94 @@
+const { expect } = require('chai');
+const Checkit = require('../../../shared/Checkit');
+
+describe('Debt Collective Checkit extensions', () => {
+  describe('social security number', () => {
+    const checkit = new Checkit({ social: ['ssn'] });
+    it('should handle numbers', () => {
+      const [err, res] = checkit.validateSync({ social: 123121234 });
+      expect(err).null;
+      expect(res).exist;
+    });
+
+    it('should handle strings', () => {
+      const [err, res] = checkit.validateSync({ social: '123121234' });
+      expect(err).null;
+      expect(res).exist;
+    });
+
+    it('should not allow letters', () => {
+      const [err, res] = checkit.validateSync({ social: 'asdasasdf' });
+      expect(err).exist;
+      expect(res).null;
+    });
+
+    it('should return a meaningful error', () => {
+      const [{ errors }, res] = checkit.validateSync({ social: 0 });
+      expect(errors).exist;
+      expect(errors.social).exist;
+      expect(errors.social.message).eq('Invalid social security number');
+      expect(res).null;
+    });
+
+    it('should allow dashes', () => {
+      const [err, res] = checkit.validateSync({ social: '123-12-1234' });
+      expect(err).to.be.null;
+      expect(res).exist;
+    });
+  });
+
+  describe('oneOf', () => {
+    const checkit = new Checkit({ enumed: ['oneOf:Hello, world, MONSTERS'] });
+    it('should pass when the option is valid', () => {
+      const [err, res] = checkit.validateSync({ enumed: 'Hello' });
+      expect(err).null;
+      expect(res).exist;
+    });
+
+    it('should not pass when the option is invalid', () => {
+      const [err, res] = checkit.validateSync({ enumed: 'Blarg' });
+      expect(err).exist;
+      expect(res).null;
+    });
+
+    it('should return a meaningful error message', () => {
+      const [{ errors }, res] = checkit.validateSync({ enumed: 'Blarg' });
+      expect(errors).exist;
+      expect(errors.enumed).exist;
+      expect(errors.enumed.message).eq('Invalid option. Was not one of Hello, world, MONSTERS');
+      expect(res).null;
+    });
+
+    it('should be case sensitive by default', () => {
+      const [err, res] = checkit.validateSync({ enumed: 'monsters' });
+      expect(err).exist;
+      expect(res).null;
+    });
+
+    it('should allow case insensitivity', () => {
+      const [err, res] = new Checkit({
+        enumed: ['oneOf: Hello, world, MONSTERS:false'],
+      }).validateSync({ enumed: 'monsters' });
+      expect(err).null;
+      expect(res).exist;
+    });
+
+    it('should trim whitespace from the passed in value', () => {
+      const [err, res] = checkit.validateSync({ enumed: '            Hello           ' });
+      expect(err).null;
+      expect(res).exist;
+    });
+
+    it('should handle array input', () => {
+      const [err, res] = checkit.validateSync({ enumed: ['Hello', 'MONSTERS'] });
+      expect(err).null;
+      expect(res).exist;
+    });
+
+    it('should pass on empty array', () => {
+      const [err, res] = checkit.validateSync({ enumed: [] });
+      expect(err).null;
+      expect(res).exist;
+    });
+  });
+});
