576a159c9b9206731ad8ce421659cd54acfdd4cd
Alexandre Marcondes <alex@marcondesian.net>
2018-04-14 15:49:44 -0400

    Allow all numeric types (to include strings of numbers from form-data)

--- tests/utils/form-validation-suites/field-validation-suites/currency.js
+++ tests/utils/form-validation-suites/field-validation-suites/currency.js
@@ -7,7 +7,7 @@ const expectGreaterThan = expectRule('greaterThan');
 module.exports = (fieldName, getDispute, isRequired = true) => {
   required(fieldName, getDispute, isRequired);
 
-  num(fieldName, getDispute);
+  num(fieldName, getDispute, true);
 
   it('should not be less than 0', performTest(getDispute, fieldName, -1, expectGreaterThan));
   it('should allow greater than 0', performTest(getDispute, fieldName, 1, expectGreaterThan, true));
