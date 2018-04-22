a2fa86f0e147b890e2077b7c8632019329316a67
Alexandre Marcondes <alex@marcondesian.net>
2018-04-22 13:07:59 -0400

    Clean up authentication tests

--- services/auth/tests.js
+++ services/auth/tests.js
@@ -1,8 +1,6 @@
-const t = {
+const { isDisputeAdmin, isDisputeOwner } = (module.exports = {
   isDisputeAdmin: ({ user }) => user && (user.admin || user.groups.includes('dispute-admin')),
   loggedIn: ({ user }) => user !== undefined,
   isDisputeOwner: ({ dispute, user }) => user.id === dispute.userId,
-  ownerOrAdmin: req => t.isDisputeAdmin(req) || t.isDisputeOwner(req),
-};
-
-module.exports = t;
+  ownerOrAdmin: req => [isDisputeAdmin, isDisputeOwner].some(test => test(req)),
+});
