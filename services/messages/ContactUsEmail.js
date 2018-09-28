a41b0d4c09b3e3f4f3b8d651ec486c35e7c2634b
Alexandre Marcondes <alex@marcondesian.net>
2018-09-27 22:48:48 -0700

    Send contact us message for admins through discourse

--- services/messages/ContactUsEmail.js
+++ services/messages/ContactUsEmail.js
@@ -1,29 +1,29 @@
 const {
   mailers: { contactEmail },
+  discourse: { adminRole },
 } = require('$config/config');
 const DebtCollectiveMessage = require('./DebtCollectiveMessage');
+const DiscourseMessage = require('./DiscourseMessage');
 
 /**
- * Sends contact us emails to the Debt Collective organizers
+ * Sends contact us message to the Debt Collective organizers
  */
-class ForAdmin extends DebtCollectiveMessage {
+class ForAdmin extends DiscourseMessage {
   /**
    * @param {string} message Message for the Debt Collective organizers
    * @param {string} email The email address of the visitor contacting the Debt Collective
    * @param {string} name The name of the visitor contact the Debt Collective
    */
   constructor(message, email, name) {
-    super('ContactUsEmail.ForAdmin', {
-      to: ForAdmin.to,
-      // Must send 'from' the contact email as SES requires emails to be verified
-      // The name may remain the name of the visitor, however.
-      from: `${name} <${contactEmail}>`,
+    super('ContactUsEmail.ForAdmin', null, {
+      to: [adminRole],
       subject: `${name} has sent a message for the organizers`,
     });
 
     this.locals = {
       message,
       name,
+      email,
     };
   }
 }
@@ -52,7 +52,7 @@ class ForVisitor extends DebtCollectiveMessage {
 }
 
 ForVisitor.subject = 'Thank you for contacting us...';
-ForAdmin.to = ForVisitor.from = `The Debt Collective Organizers <${contactEmail}>`;
+ForVisitor.from = `The Debt Collective Organizers <${contactEmail}>`;
 
 /**
  * Wraps the ForVisitor and ForAdmin emails to make sending both cleaner
---
+++ views/messages/ContactUsEmail.ForAdmin.md.js
@@ -0,0 +1,10 @@
+module.exports = ({ name, message, email }) =>
+  `### ${name} has a message for the Debt Collective organizers:
+
+\`\`\`
+${message}
+\`\`\`
+
+They have requested that you contact them at ${email}
+
+`;
