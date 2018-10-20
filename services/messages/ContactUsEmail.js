4062218b8df0a5ba9cb56dcb2e466f857014f967
Orlando Del Aguila <orlando@hashlabs.com>
2018-10-20 00:04:46 -0500

    removing Contact Us integration with Discourse, adding recaptcha

--- services/messages/ContactUsEmail.js
+++ services/messages/ContactUsEmail.js
@@ -1,47 +1,22 @@
 const {
   mailers: { contactEmail },
-  discourse: { adminRole },
 } = require('$config/config');
 const DebtCollectiveMessage = require('./DebtCollectiveMessage');
-const DiscourseMessage = require('./DiscourseMessage');
 
 /**
- * Sends contact us message to the Debt Collective organizers
+ * Sends contact us emails to the Debt Collective organizers
  */
-class ForAdmin extends DiscourseMessage {
+class ForAdmin extends DebtCollectiveMessage {
   /**
    * @param {string} message Message for the Debt Collective organizers
    * @param {string} email The email address of the visitor contacting the Debt Collective
    * @param {string} name The name of the visitor contact the Debt Collective
    */
   constructor(message, email, name) {
-    super('ContactUsEmail.ForAdmin', null, {
-      to: [adminRole],
-      subject: `${name} has sent a message for the organizers`,
-    });
-
-    this.locals = {
-      message,
-      name,
-      email,
-    };
-  }
-}
-
-/**
- * Sends a confirmation message to the visitor attempting to contact the organizers
- */
-class ForVisitor extends DebtCollectiveMessage {
-  /**
-   * @param {string} message Message for the Debt Collective organizers
-   * @param {string} email The email address of the visitor contacting the Debt Collective
-   * @param {string} name The name of the visitor contact the Debt Collective
-   */
-  constructor(message, email, name) {
-    super('ContactUsEmail.ForVisitor', {
-      to: `${name} <${email}>`,
-      from: ForVisitor.from,
-      subject: ForVisitor.subject,
+    super('ContactUsEmail.ForAdmin', {
+      to: ForAdmin.to,
+      from: `${name} <${email}>`,
+      subject: `${name} has sent a message using the Contact Us form`,
     });
 
     this.locals = {
@@ -51,8 +26,7 @@ class ForVisitor extends DebtCollectiveMessage {
   }
 }
 
-ForVisitor.subject = 'Thank you for contacting us...';
-ForVisitor.from = `The Debt Collective Organizers <${contactEmail}>`;
+ForAdmin.to = `The Debt Collective Organizers <${contactEmail}>`;
 
 /**
  * Wraps the ForVisitor and ForAdmin emails to make sending both cleaner
@@ -65,19 +39,17 @@ class ContactUsEmail {
    */
   constructor(message, email, name) {
     this.forAdmin = new ForAdmin(message, email, name);
-    this.forVisitor = new ForVisitor(message, email, name);
   }
 
   send() {
-    return Promise.all([this.forAdmin.send(), this.forVisitor.send()]);
+    return this.forAdmin.send();
   }
 
   toString() {
-    return `${this.forAdmin.toString()}\n${this.forVisitor.toString()}`;
+    return `${this.forAdmin.toString()}`;
   }
 }
 
 ContactUsEmail.ForAdmin = ForAdmin;
-ContactUsEmail.ForVisitor = ForVisitor;
 
 module.exports = ContactUsEmail;
--- tests/unit/messages/ContactUsEmail.ForAdminTests.js
+++ tests/unit/messages/ContactUsEmail.ForAdminTests.js
@@ -4,32 +4,33 @@ const {
   mailers: { contactEmail },
 } = require('$config/config');
 
-const { ForVisitor } = ContactUsEmail;
+const { ForAdmin } = ContactUsEmail;
 
-describe('ContactUsEmail.ForVisitor', () => {
+describe('ContactUsEmail.ForAdmin', () => {
   describe('constructor', () => {
-    it('should set the from address to the contact email', () => {
-      const email = new ForVisitor('a message', 'visitor@example.com', 'Veronica Visitor');
-      expect(email.from).exist;
-      expect(email.from.slice(-(contactEmail.length + 2))).eq(`<${contactEmail}>`);
+    it('should set the to address to the contact email', () => {
+      const email = new ForAdmin('a message', 'visitor@example.com', 'Veronica Visitor');
+      expect(email.to).exist;
+      expect(email.to.slice(-(contactEmail.length + 2))).eq(`<${contactEmail}>`);
     });
 
-    it("should set the to address to the visitor's", () => {
-      const email = new ForVisitor('a message', 'visitor@example.com', 'Veronica Visitor');
-      expect(email.to).exist;
-      expect(email.to).eq('Veronica Visitor <visitor@example.com>');
+    it("should set the from address to the visitor's", () => {
+      const email = new ForAdmin('a message', 'visitor@example.com', 'Veronica Visitor');
+      expect(email.from).exist;
+      // It has to be the contact email so that SES doesn't get upset we're sending emails from unverified addresses
+      expect(email.from).eq('Veronica Visitor <visitor@example.com>');
     });
 
     it('should set the locals with the passed in message', () => {
       const message = 'a message for the organizers';
-      const email = new ForVisitor(message, '', '');
+      const email = new ForAdmin(message, '', '');
       expect(email.locals.message).exist;
       expect(email.locals.message).eq(message);
     });
 
     it('should set the locals with the passed in name of the visitor', () => {
       const name = 'Veronica Visitor';
-      const email = new ForVisitor('', '', name);
+      const email = new ForAdmin('', '', name);
       expect(email.locals.name).exist;
       expect(email.locals.name).eq(name);
     });
@@ -37,7 +38,7 @@ describe('ContactUsEmail.ForVisitor', () => {
 
   describe('render', () => {
     it('should be able to render', () => {
-      const email = new ForVisitor(
+      const email = new ForAdmin(
         'a message for the organizers',
         'visitor@example.com',
         'Veronica Visitor',
@@ -45,7 +46,7 @@ describe('ContactUsEmail.ForVisitor', () => {
       const html = email.render();
       expect(html).exist;
       expect(html).include(email.locals.message);
-      expect(html).include('You have sent the following message');
+      expect(html).include(email.locals.name);
     });
   });
 });
