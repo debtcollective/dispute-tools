589068eb72e60c46f514e9cb395cf6b08a26d73e
Orlando Del Aguila <orlando@hashlabs.com>
2020-03-05 11:14:28 -0600

    dev(CleanupDisputes): define queuescheduler

--- jobs/scheduled/CleanupDisputes.js
+++ jobs/scheduled/CleanupDisputes.js
@@ -1,7 +1,23 @@
-import { Worker } from 'bullmq';
+import { Worker, QueueScheduler } from 'bullmq';
 
-const worker = new Worker('cleanupDisputes', async job => {
-  console.log(job.data);
+const queueName = 'CleanupDisputes';
+const queue = new QueueScheduler(queueName);
+
+// Repeat job once every day at 3:15 (am)
+queue.add(
+  queueName,
+  { name: queueName },
+  {
+    repeat: {
+      cron: '15 3 * * *',
+    },
+  },
+);
+
+const worker = new Worker(queueName, async job => {
+  // get all disputes older than 30 days in New status
+  // for each dispute run dispute.delete()
+  console.log(job);
 });
 
 export default worker;
